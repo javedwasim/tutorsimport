82e10338260571db6790a5b176cb6965063b3559
javed wasim <javedafaq@gmail.com>
2018-01-21 15:31:09 +0500

    implement job queue with mail trap api

---
+++ app/Console/Commands/SendEmails.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class SendEmails extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'notify:email';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send drip e-mails to a user';
+
+    /**
+     * The drip email service
+     *
+     * @var DripEmailer
+     */
+
+    protected $drip;
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        echo "here";
+    }
+}
